527fa2343dbb80d9be8dd413a1de6fe3db6682c4
Vlad Stoian <epicrose@yahoo.com>
2011-03-20 20:50:04 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloggy::Application.config.secret_token = '87616173f44df458f3ac911db31050a2542fdd1782369bcb514bad05947ff6db996c1a32290e79c8bdee9c2fb20a6683e5d7eb24ccc35a3c0c73de40dd2bdb47'
